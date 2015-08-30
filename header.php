b63a6381df82ab6a78b84817bf9bcb77d4b73ba0
svmalvaez <svmalvaez@gmail.com>
2015-08-30 13:28:37 -0500

    Align nav menu and logo

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
     <header <?php header_class(); ?>>
     <div class="container">
       <div class="row">
-        <div class="col-xs-12">
+        <div class="col-xs-3">
           <div class="site-branding">
             <?php
             global $infinity_custom_logo;
@@ -45,9 +45,9 @@
             <?php } ?>
           </div>
         </div>
-        <div class="col-xs-12">
-          <div class="row middle-sm">
-            <div class="last-sm first-lg col-sm-3 col-lg-8">
+        <div class="col-xs-9">
+          <div class="row middle-sm site-branding">
+            <div class="last-sm first-lg col-sm-3 col-lg-12">
               <button class="menu-link"><span class="lines"></span></button>
                 <?php
                 if ( class_exists( 'TM_Walker_Nav_Menu' ) && has_nav_menu( 'primary' ) ) {
@@ -80,27 +80,6 @@
               <?php  }
                 ?>
             </div>
-            <div class="col-sm-6 col-lg-2">
-              <div class="social">
-                <?php
-                if ( has_nav_menu( 'social' ) ) {
-                  wp_nav_menu(
-                    array(
-                      'theme_location' => 'social',
-                      'fallback_cb' => false
-                    )
-                  );
-                } ?>
-              </div>
-            </div>
-            <?php if(Kirki::get_option( 'infinity', 'header_search_enable')) { ?>
-              <div class="col-sm-3 col-lg-2">
-                <div class="search-box">
-                  <?php get_search_form(); ?>
-                  <i class="fa fa-search"></i>
-                </div>
-              </div>
-            <?php } ?>
           </div>
         </div>
       </div>
