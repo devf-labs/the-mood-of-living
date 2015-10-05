bfa53622ef08a1c20f8971367b5e287626aae1cb
Henoc Díaz <henocdz@gmail.com>
2015-10-05 01:43:42 -0500

    404 page fixed

--- 404.php
+++ 404.php
@@ -1,39 +1,36 @@
 <?php
 /**
- * The template for displaying 404 pages (not found).
+ * The template for displaying 404 pages (Not Found).
  *
- *  @package ThemeMove
+ * @package WordPress
+ * @subpackage Razzo
+ * @since Razzo 1.0
  */
-$layout = Kirki::get_option( 'infinity', 'site_layout' );
+
 get_header(); ?>
 
-<div class="content-wrapper">
-  <div class="container">
-    <div class="row">
-      <?php if('sidebar-content' == $layout){ ?>
-        <?php get_sidebar(); ?>
-      <?php } ?>
-      <?php if ('sidebar-content' == $layout  || 'content-sidebar' == $layout) { ?>
-        <?php $class = 'col-md-9'; ?>
-      <?php } else { ?>
-        <?php $class = 'col-md-12'; ?>
-      <?php } ?>
-      <div class="<?php echo esc_attr($class); ?>">
-        <main id="main" class="site-main">
+  <div id="main" class="clearfix">
 
-          <section class="error-404 not-found">
-            <header class="page-header">
-              <h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'infinity' ); ?></h1>
-            </header><!-- .page-header -->
+  <div id="primary">
+    <div id="content" role="main">
 
-          </section><!-- .error-404 -->
-        </main><!-- #main -->
-      </div>
-      <?php if('content-sidebar' == $layout){ ?>
-        <?php get_sidebar(); ?>
-      <?php } ?>
-    </div>
-  </div>
-</div><!-- .content-wrapper  -->
+      <article id="post-0" class="post error404 not-found">
+        <header class="entry-header">
+          <h1 class="entry-title"><?php _e( '404', 'mega' ); ?></h1>
+        </header>
 
-<?php get_footer(); ?>
+        <div class="entry-content">
+          <h2><?php _e( 'Unfortunately the page you tried to access was unavailable due to an error.', 'mega' ); ?></h2>
+          
+          <p>
+          <?php _e( 'Please', 'mega' ); ?>
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" id="back">
+              <?php _e( 'click here', 'mega' ); ?>
+              <?php _e( 'to return to the homepage.', 'mega' ); ?>
+            </a>
+          </p>
+        </div><!-- .entry-content -->
+      </article><!-- #post-0 -->
+
+    </div><!-- #content -->
+  </div><!-- #primary -->
